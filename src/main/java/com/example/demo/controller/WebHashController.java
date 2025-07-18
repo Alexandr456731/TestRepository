f92820dc680d17582b875cc03a22f371af275ce7
Alexandr Burnaev <aleksandrburnaev456@gmail.com>
2025-07-18 19:18:49 +0300

    q

--- src/main/java/com/example/demo/controller/WebHashController.java
+++ src/main/java/com/example/demo/controller/WebHashController.java
@@ -1,22 +1,20 @@
 package com.example.demo.controller;
 
-import com.example.demo.repository.UserRepository;
 import com.example.demo.service.WebHashService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WebHash {
-    private final WebHashService webHashService;
+public class WebHashController {
+    private final WebHashService webHash;
 
-    public WebHash(WebHashService webHashService){
-        this.webHashService = webHashService;
+    public WebHashController(WebHashService webHash){
+        this.webHash = webHash;
     }
 
     @GetMapping("/hash-admin/{login}/{hash}")
     public String getHash(@PathVariable String login, @PathVariable String hash) {
-        return webHashService.getHash(login, hash);
+        return webHash.getHash(login, hash);
     }
 }
