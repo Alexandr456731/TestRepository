96d67501e991baa7b294bd307023cfadf891f7e3
Alexandr Burnaev <aleksandrburnaev456@gmail.com>
2025-07-18 19:15:23 +0300

    q

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query(value = "SELECT * FROM users WHERE login = :login", nativeQuery = true)
+    @Query(value = "FROM User WHERE login = :login")
     Optional<User> findByLogin(String login);
 
-    @Query(value = "SELECT * FROM users WHERE email = :email", nativeQuery = true)
+    @Query(value = "FROM User WHERE email = :email")
     Optional<User> findByEmail(String email);
 }
